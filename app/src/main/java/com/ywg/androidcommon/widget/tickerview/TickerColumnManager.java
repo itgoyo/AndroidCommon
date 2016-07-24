4a213c33e73b2f9b431885c1e1feb627adea7ad1
ywg228 <1107779144@qq.com>
2016-07-24 18:48:40 +0800

    commit

---
+++ app/src/main/java/com/ywg/androidcommon/widget/tickerview/TickerColumnManager.java
@@ -0,0 +1,147 @@
+package com.ywg.androidcommon.widget.tickerview;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * In ticker, each character in the rendered text is represented by a {@link TickerColumn}. The
+ * column can be seen as a column of text in which we can animate from one character to the next
+ * by scrolling the column vertically. The {@link TickerColumnManager} is then a
+ * manager/convenience class for handling a list of {@link TickerColumn} which then combines into
+ * the entire string we are rendering.
+ *
+ * @author Jin Cao, Robinhood
+ */
+class TickerColumnManager {
+    final ArrayList<TickerColumn> tickerColumns = new ArrayList<>();
+    private final TickerDrawMetrics metrics;
+
+    // The character list that dictates how to transition from one character to another.
+    private char[] characterList;
+    // A minor optimization so that we can cache the indices of each character.
+    private Map<Character, Integer> characterIndicesMap;
+
+    TickerColumnManager(TickerDrawMetrics metrics) {
+        this.metrics = metrics;
+    }
+
+    /**
+     * @see {@link TickerView#setCharacterList(char[])}.
+     */
+    void setCharacterList(char[] characterList) {
+        this.characterList = characterList;
+        this.characterIndicesMap = new HashMap<>(characterList.length);
+
+        for (int i = 0; i < characterList.length; i++) {
+            characterIndicesMap.put(characterList[i], i);
+        }
+    }
+
+    /**
+     * @return whether or not {@param text} should be debounced because it's the same as the
+     *         current target text of this column manager.
+     */
+    boolean shouldDebounceText(char[] text) {
+        final int newTextSize = text.length;
+        if (newTextSize == tickerColumns.size()) {
+            for (int i = 0; i < newTextSize; i++) {
+                if (text[i] != tickerColumns.get(i).getTargetChar()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Tell the column manager the new target text that it should display.
+     */
+    boolean setText(char[] text, boolean animate) {
+        if (characterList == null) {
+            throw new IllegalStateException("Need to call setCharacterList(char[]) first.");
+        }
+
+        final int newTextSize = text.length;
+        if (animate) {
+            // If we are animating, we don't want to remove old columns yet.
+            insertColumnsUpTo(newTextSize);
+        } else {
+            ensureColumnSize(newTextSize);
+        }
+
+        final int columnsSize = tickerColumns.size();
+        for (int i = 0; i < columnsSize; i++) {
+            tickerColumns.get(i).setTargetChar(i < newTextSize ? text[i] : TickerUtils.EMPTY_CHAR);
+        }
+
+        return true;
+    }
+
+    void setAnimationProgress(float animationProgress) {
+        for (int i = 0, size = tickerColumns.size(); i < size; i++) {
+            final TickerColumn column = tickerColumns.get(i);
+            column.setAnimationProgress(animationProgress);
+        }
+    }
+
+    float getMinimumRequiredWidth() {
+        float width = 0f;
+        for (int i = 0, size = tickerColumns.size(); i < size; i++) {
+            width += tickerColumns.get(i).getMinimumRequiredWidth();
+        }
+        return width;
+    }
+
+    float getCurrentWidth() {
+        float width = 0f;
+        for (int i = 0, size = tickerColumns.size(); i < size; i++) {
+            width += tickerColumns.get(i).getCurrentWidth();
+        }
+        return width;
+    }
+
+    /**
+     * This method will draw onto the canvas the appropriate UI state of each column dictated
+     * by {@param animationProgress}. As a side effect, this method will also translate the canvas
+     * accordingly for the draw procedures.
+     */
+    void draw(Canvas canvas, Paint textPaint) {
+        for (int i = 0, size = tickerColumns.size(); i < size; i++) {
+            final TickerColumn column = tickerColumns.get(i);
+            column.draw(canvas, textPaint);
+            canvas.translate(column.getCurrentWidth(), 0f);
+        }
+    }
+
+    /**
+     * Ensure that the number of columns matches {@param targetSize}.
+     */
+    void ensureColumnSize(int targetSize) {
+        final int columnSize = tickerColumns.size();
+        if (targetSize > columnSize) {
+            insertColumnsUpTo(targetSize);
+        } else {
+            for (int i = 0; i < columnSize - targetSize; i++) {
+                tickerColumns.remove(tickerColumns.size() - 1);
+            }
+        }
+    }
+
+    /**
+     * Insert (if applicable) columns until the number of columns match {@param targetSize}.
+     */
+    void insertColumnsUpTo(int targetSize) {
+        final int currentSize = tickerColumns.size();
+        if (targetSize > currentSize) {
+            final int toInsert = targetSize - currentSize;
+            for (int i = 0; i < toInsert; i++) {
+                tickerColumns.add(new TickerColumn(characterList, characterIndicesMap, metrics));
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ywg/androidcommon/widget/tickerview/TickerView.java
@@ -0,0 +1,426 @@
+package com.ywg.androidcommon.widget.tickerview;
+
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+
+import com.ywg.androidcommon.R;
+
+public class TickerView extends View {
+    private static final int DEFAULT_TEXT_SIZE = 12;
+    private static final int DEFAULT_TEXT_COLOR = Color.BLACK;
+    private static final int DEFAULT_ANIMATION_DURATION = 350;
+    private static final Interpolator DEFAULT_ANIMATION_INTERPOLATOR =
+            new AccelerateDecelerateInterpolator();
+    private static final int DEFAULT_GRAVITY = Gravity.START;
+
+    protected final Paint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+
+    private final TickerDrawMetrics metrics = new TickerDrawMetrics(textPaint);
+    private final TickerColumnManager columnManager = new TickerColumnManager(metrics);
+
+    // Minor optimizations for re-positioning the canvas for the composer.
+    private final Rect viewBounds = new Rect();
+
+    private ValueAnimator animator;
+
+    // View attributes, defaults are set in init().
+    private float textSize;
+    private int textColor;
+    private long animationDurationInMillis;
+    private Interpolator animationInterpolator;
+    private int gravity;
+
+    public TickerView(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public TickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public TickerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public TickerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * We currently only support the following set of XML attributes:
+     * <ul>
+     *     <li>app:textColor
+     *     <li>app:textSize
+     * </ul>
+     */
+    protected void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        final Resources res = context.getResources();
+
+        // Set the view attributes from XML or from default values defined in this class
+        final TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.ticker_TickerView,
+                defStyleAttr, defStyleRes);
+
+        final int textColor = arr.getColor(R.styleable.ticker_TickerView_ticker_textColor,
+                DEFAULT_TEXT_COLOR);
+        setTextColor(textColor);
+        final float textSize = arr.getDimension(R.styleable.ticker_TickerView_ticker_textSize,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, DEFAULT_TEXT_SIZE,
+                        res.getDisplayMetrics()));
+        setTextSize(textSize);
+
+        animationDurationInMillis = arr.getInt(
+                R.styleable.ticker_TickerView_ticker_animationDuration, DEFAULT_ANIMATION_DURATION);
+        animationInterpolator = DEFAULT_ANIMATION_INTERPOLATOR;
+        gravity = arr.getInt(R.styleable.ticker_TickerView_android_gravity, DEFAULT_GRAVITY);
+
+        arr.recycle();
+
+        animator = ValueAnimator.ofFloat(1f);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                columnManager.setAnimationProgress(animation.getAnimatedFraction());
+                invalidate();
+            }
+        });
+    }
+
+
+    /********** BEGIN PUBLIC API **********/
+
+
+    /**
+     * This is the primary API that the view uses to determine how to animate from one character
+     * to another. The provided character array dictates what characters will appear between
+     * the start and end characters.
+     *
+     * <p>For example, given the list [a,b,c,d,e], if the view wants to animate from 'd' to 'a',
+     * it will know that it has to go from 'd' to 'c' to 'b' to 'a', and these are the characters
+     * that show up during the animation scroll.
+     *
+     * <p>You can find some helpful character list generators in {@link TickerUtils}.
+     *
+     * <p>Special note: the character list must contain {@link TickerUtils#EMPTY_CHAR} because the
+     * ticker needs to know how to animate from empty to another character (e.g. when the length
+     * of the string changes).
+     *
+     * @param characterList the character array that dictates character orderings.
+     */
+    public void setCharacterList(char[] characterList) {
+        boolean foundEmpty = false;
+        for (char character : characterList) {
+            if (character == TickerUtils.EMPTY_CHAR) {
+                foundEmpty = true;
+                break;
+            }
+        }
+
+        if (!foundEmpty) {
+            throw new IllegalArgumentException("Missing TickerUtils#EMPTY_CHAR in character list");
+        }
+
+        columnManager.setCharacterList(characterList);
+    }
+
+    /**
+     * Sets the string value to display. If the TickerView is currently empty, then this method
+     * will immediately display the provided text. Otherwise, it will run the default animation
+     * to reach the provided text.
+     *
+     * @param text the text to display.
+     */
+    public void setText(String text) {
+        setText(text, columnManager.getCurrentWidth() > 0);
+    }
+
+    /**
+     * Similar to {@link #setText(String)} but provides the optional argument of whether to
+     * animate to the provided text or not.
+     *
+     * @param text the text to display.
+     * @param animate whether to animate to text.
+     */
+    public synchronized void setText(String text, boolean animate) {
+        final char[] targetText = text == null ? new char[0] : text.toCharArray();
+
+        if (columnManager.shouldDebounceText(targetText)) {
+            return;
+        }
+
+        columnManager.setText(targetText, animate);
+        columnManager.setAnimationProgress(animate ? 0f : 1f);
+        setContentDescription(text);
+        checkForRelayout();
+
+        if (animate) {
+            // Kick off the animator that draws the transition
+            if (animator.isRunning()) {
+                animator.cancel();
+            }
+
+            animator.setDuration(animationDurationInMillis);
+            animator.setInterpolator(animationInterpolator);
+            animator.start();
+        } else {
+            invalidate();
+        }
+    }
+
+    /**
+     * @return the current text color that's being used to draw the text.
+     */
+    public int getTextColor() {
+        return textColor;
+    }
+
+    /**
+     * Sets the text color used by this view. The default text color is defined by
+     * {@link #DEFAULT_TEXT_COLOR}.
+     *
+     * @param color the color to set the text to.
+     */
+    public void setTextColor(int color) {
+        if (this.textColor != color) {
+            textColor = color;
+            textPaint.setColor(textColor);
+            invalidate();
+        }
+    }
+
+    /**
+     * @return the current text size that's being used to draw the text.
+     */
+    public float getTextSize() {
+        return textSize;
+    }
+
+    /**
+     * Sets the text size used by this view. The default text size is defined by
+     * {@link #DEFAULT_TEXT_SIZE}.
+     *
+     * @param textSize the text size to set the text to.
+     */
+    public void setTextSize(float textSize) {
+        if (this.textSize != textSize) {
+            this.textSize = textSize;
+            textPaint.setTextSize(textSize);
+            onTextPaintMeasurementChanged();
+        }
+    }
+
+    /**
+     * @return the current text typeface.
+     */
+    public Typeface getTypeface() {
+        return textPaint.getTypeface();
+    }
+
+    /**
+     * Sets the typeface size used by this view.
+     *
+     * @param typeface the typeface to use on the text.
+     */
+    public void setTypeface(Typeface typeface) {
+        textPaint.setTypeface(typeface);
+        onTextPaintMeasurementChanged();
+    }
+
+    /**
+     * @return the duration in milliseconds that the transition animations run for.
+     */
+    public long getAnimationDuration() {
+        return animationDurationInMillis;
+    }
+
+    /**
+     * Sets the duration in milliseconds that this TickerView runs its transition animations. The
+     * default animation duration is defined by {@link #DEFAULT_ANIMATION_DURATION}.
+     *
+     * @param animationDurationInMillis the duration in milliseconds.
+     */
+    public void setAnimationDuration(long animationDurationInMillis) {
+        this.animationDurationInMillis = animationDurationInMillis;
+    }
+
+    /**
+     * @return the interpolator used to interpolate the animated values.
+     */
+    public Interpolator getAnimationInterpolator() {
+        return animationInterpolator;
+    }
+
+    /**
+     * Sets the interpolator for the transition animation. The default interpolator is defined by
+     * {@link #DEFAULT_ANIMATION_INTERPOLATOR}.
+     *
+     * @param animationInterpolator the interpolator for the animation.
+     */
+    public void setAnimationInterpolator(Interpolator animationInterpolator) {
+        this.animationInterpolator = animationInterpolator;
+    }
+
+    /**
+     * @return the current text gravity used to align the text. Should be one of the values defined
+     *         in {@link android.view.Gravity}.
+     */
+    public int getGravity() {
+        return gravity;
+    }
+
+    /**
+     * Sets the gravity used to align the text.
+     *
+     * @param gravity the new gravity, should be one of the values defined in
+     *                {@link android.view.Gravity}.
+     */
+    public void setGravity(int gravity) {
+        if (this.gravity != gravity) {
+            this.gravity = gravity;
+            invalidate();
+        }
+    }
+
+
+    /********** END PUBLIC API **********/
+
+
+    /**
+     * Force the view to call {@link #requestLayout()} if the new text doesn't match the old bounds
+     * we set for the previous view state.
+     */
+    private void checkForRelayout() {
+        if (getWidth() != computeDesiredWidth() || getHeight() != computeDesiredHeight()) {
+            requestLayout();
+        }
+    }
+
+    private int computeDesiredWidth() {
+        return (int) columnManager.getMinimumRequiredWidth() + getPaddingLeft() + getPaddingRight();
+    }
+
+    private int computeDesiredHeight() {
+        return (int) metrics.getCharHeight() + getPaddingTop() + getPaddingBottom();
+    }
+
+    /**
+     * Re-initialize all of our variables that are dependent on the TextPaint measurements.
+     */
+    private void onTextPaintMeasurementChanged() {
+        metrics.invalidate();
+        checkForRelayout();
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int desiredWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int desiredHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        switch (widthMode) {
+            case MeasureSpec.EXACTLY:
+                break;
+            case MeasureSpec.AT_MOST:
+                desiredWidth = Math.min(desiredWidth, computeDesiredWidth());
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                desiredWidth = computeDesiredWidth();
+                break;
+        }
+
+        switch (heightMode) {
+            case MeasureSpec.EXACTLY:
+                break;
+            case MeasureSpec.AT_MOST:
+                desiredHeight = Math.min(desiredHeight, computeDesiredHeight());
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                desiredHeight = computeDesiredHeight();
+                break;
+        }
+
+        setMeasuredDimension(desiredWidth, desiredHeight);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldw, int oldh) {
+        super.onSizeChanged(width, height, oldw, oldh);
+        viewBounds.set(getPaddingLeft(), getPaddingTop(), width - getPaddingRight(),
+                height - getPaddingBottom());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.save();
+
+        realignAndClipCanvasForGravity(canvas);
+
+        // canvas.drawText writes the text on the baseline so we need to translate beforehand.
+        canvas.translate(0f, metrics.getCharBaseline());
+
+        columnManager.draw(canvas, textPaint);
+
+        canvas.restore();
+    }
+
+    private void realignAndClipCanvasForGravity(Canvas canvas) {
+        final float currentWidth = columnManager.getCurrentWidth();
+        final float currentHeight = metrics.getCharHeight();
+        realignAndClipCanvasForGravity(canvas, gravity, viewBounds, currentWidth, currentHeight);
+    }
+
+    // VisibleForTesting
+    static void realignAndClipCanvasForGravity(Canvas canvas, int gravity, Rect viewBounds,
+            float currentWidth, float currentHeight) {
+        final int availableWidth = viewBounds.width();
+        final int availableHeight = viewBounds.height();
+
+        float translationX = 0;
+        float translationY = 0;
+        if ((gravity & Gravity.CENTER_VERTICAL) == Gravity.CENTER_VERTICAL) {
+            translationY = viewBounds.top + (availableHeight - currentHeight) / 2f;
+        }
+        if ((gravity & Gravity.CENTER_HORIZONTAL) == Gravity.CENTER_HORIZONTAL) {
+            translationX = viewBounds.left + (availableWidth - currentWidth) / 2f;
+        }
+        if ((gravity & Gravity.TOP) == Gravity.TOP) {
+            translationY = 0;
+        }
+        if ((gravity & Gravity.BOTTOM) == Gravity.BOTTOM) {
+            translationY = viewBounds.top + (availableHeight - currentHeight);
+        }
+        if ((gravity & Gravity.START) == Gravity.START) {
+            translationX = 0;
+        }
+        if ((gravity & Gravity.END) == Gravity.END) {
+            translationX = viewBounds.left + (availableWidth - currentWidth);
+        }
+
+        canvas.translate(translationX ,translationY);
+        canvas.clipRect(0f, 0f, currentWidth, currentHeight);
+    }
+}
